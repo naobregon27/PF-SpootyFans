64c49c899acd9c6e2830070389e8beba9c5d9f28
Ezequiel Benitez <125520493+Eze-quielph@users.noreply.github.com>
2023-07-31 16:34:45 -0300

    Create configure-base Cloudinary

---
+++ Server/src/helpers/uploadMusic.js
@@ -0,0 +1,28 @@
+const {
+  cloudinary,
+  CLOUDINARY_URL,
+} = require("../../config/cloudinary.config");
+
+const uploadMusic = async (file) => {
+  cloudinary
+    .uploader(file, {
+      resource_type: "sound",
+      public_id: "myfolder/mysubfolder/dog_closeup",
+      chunk_size: 6000000,
+      eager: [
+        { width: 300, height: 300, crop: "pad", audio_codec: "none" },
+        {
+          width: 160,
+          height: 100,
+          crop: "crop",
+          gravity: "south",
+          audio_codec: "",
+        },
+      ],
+      eager_async: true,
+      eager_notification_url: "https://mysite.example.com/notify_endpoint",
+    })
+    .then((result) => console.log(result));
+};
+
+module.exports = uploadMusic
