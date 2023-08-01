10c89b4db4a67ac2141b28fdd1d47e9fe9f366c9
FVazquezRisso <facundovazquez321@gmail.com>
2023-08-01 12:01:09 -0300

    Arreglo de errores y archivos duplicados

--- Server/src/routes/Music.routes.js
+++ Server/src/routes/Music.routes.js
@@ -1,10 +1,23 @@
 const musicRouter = require('express').Router();
 const multer = require('multer');
-const { postMusic } = require('../controllers/music.controller'); // Importa directamente el controlador y su función
+const { postMusic } = require("../controllers/music/music.controller"); // Importa directamente el controlador y su función
+const getSongByName = require("../controllers/music/getMusicByName");
 
 // Configurar Multer para guardar el archivo temporal en la carpeta 'uploads'
-const upload = multer({ dest: 'uploads/' });
+const upload = multer({ dest: "uploads/" });
 
-musicRouter.post('/upload/url', upload.single('file'), postMusic); // Usa la función directamente sin .uploadFile
+musicRouter.post("/upload/url", upload.single("file"), postMusic); // Usa la función directamente sin .uploadFile
+
+musicRouter.get("/", async (req, res) => {
+  const { name } = req.query;
+
+  const songs = await getSongByName(name);
+
+  if (songs.error) {
+    res.status(400).json({ error: songs.error });
+  } else {
+    res.status(200).json(songs);
+  }
+});
 
 module.exports = musicRouter;
--- Server/src/routes/index.routes.js
+++ Server/src/routes/index.routes.js
@@ -1,6 +1,6 @@
 const routes = require('express').Router();
 const musicRouter = require('./Music.routes')
 
-routes.use('/musica', musicRouter)
+routes.use("/musica", musicRouter);
 
 module.exports = routes
