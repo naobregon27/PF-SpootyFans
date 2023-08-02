e6fa61e418a21a8b80e50c99b6496fa7ae17c005
T_NahuelReboledo <thiagoreboledo8@gmail.com>
2023-08-02 12:21:13 -0300

    configuracion de redux y intalacion de las depend corespondientes

---
+++ Client/src/Redux/reducer.js
@@ -0,0 +1,15 @@
+const initialState = {
+
+// cambiar esto
+
+   prueba: []
+}
+
+const reducer = (state = initialState, action) => {
+   switch (action.type) {
+         default:
+            return { ...state };
+   }
+}
+
+export default reducer;
