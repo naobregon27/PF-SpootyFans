82efe617e83eaa000c9d4a4b6290f2820d1b8b59
FVazquezRisso <facundovazquez321@gmail.com>
2023-08-02 12:40:52 -0300

    Implementación de JWT

--- Server/src/routes/User.routes.js
+++ Server/src/routes/User.routes.js
@@ -1,6 +1,7 @@
 const userRouter = require("express").Router();
 const userRegister = require("../controllers/users/userRegister");
 const userLogin = require("../controllers/users/userLogin");
+const authentication = require("../middlewares/authentication");
 
 userRouter.post("/register", async (req, res) => {
   // Extraemos los datos del body
@@ -21,14 +22,16 @@ userRouter.post("/login", async (req, res) => {
   // Extraemos los datos del body
   const userData = req.body;
   // Enviamos los datos del usuario a la función de login y almacenamos su retorno en una variable
-  const loggedUser = await userLogin(userData);
+  const token = await userLogin(userData);
 
-  if (loggedUser.error) {
+  if (token.error) {
     // Respondemos con un error en caso de que algo salga mal
-    res.status(400).json({ error: loggedUser.error });
+    res.status(400).json({ error: token.error });
   } else {
     // Respondemos con el usuario en caso de que todo salga bien
-    res.status(200).json(loggedUser);
+    res
+      .status(200)
+      .json({ message: "Usuario autenticado exitosamente.", token });
   }
 });
 
