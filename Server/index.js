7c466a3f56077656b1be51146fd928e7f589164a
naobregon27 <na.obregon27@hotmail.com>
2023-08-02 19:34:12 -0300

    config socket.io

--- Server/index.js
+++ Server/index.js
@@ -1,8 +1,21 @@
 require("dotenv").config();
 const app = require("./src/app");
-const port = process.env.PORT || 3001;
+const port = process .env.PORT || 3001;
 const { conn } = require("./src/db");
 const llenarDB = require("./llenarDB");
+
+//config socket.io
+
+const http = require ("http");
+const socketIO = require('socket.io');
+
+const server = http.createServer(app);
+const io = socketIO(server)
+
+io.on ('connection', (socket)=>{
+  console.log (`User connected`);
+})
+
 
 conn
   .sync({ force: true })
