a2bb948f78cf147a82e3a9f88ed8d3f01292cb21
Ezequiel Benitez <125520493+Eze-quielph@users.noreply.github.com>
2023-08-03 00:38:12 -0300

    create a route for searching by Id and fix model song id

--- Server/src/controllers/music/music.controller.js
+++ Server/src/controllers/music/music.controller.js
@@ -18,6 +18,23 @@ const postMusic = async (req, res) => {
   }
 };
 
+const searchId = async(req, res)=>{
+  const {id} = req.params
+  console.log(id);
+ try {
+  if(!id) throw new Error("Debe de mandarme id");
+  if(typeof id != 'string') throw new Error("El id deve ser un string");
+  if(id.length < 5) throw new Error("Tu id es muy corto, revisalo");
+  const song = await Song.findByPk(id)
+
+  res.status(200).json(song)
+ } catch (error) {
+  res.status.json({error: error.message})
+ }
+
+}
+
 module.exports = {
   postMusic,
+  searchId
 };
