21f405d5736b93aa20b7ee792b8d360ab462528c
ArielBenz1 <ariex1040p@gmail.com>
2023-08-03 00:19:53 -0300

    Añadido el detail y la pagination

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -2,6 +2,8 @@ import { Routes, Route } from "react-router-dom";
 import Landing from "./Views/Landing/Landing";
 import Form from "./Views/Form/Login";
 import Form_song from "./Views/Form_song/Form_song";
+import Detail from "./Views/Detail/Detail";
+import Home from "./Views/Home/Home";
 
 function App() {
 
@@ -10,8 +12,9 @@ function App() {
     <Routes>
       <Route exact path="/" element={<Landing/>}/>
       <Route path="/login" element={<Form />} />
-      <Route pathe="/create" element={<Form_song/>}/>
-
+      <Route path="/create" element={<Form_song/>}/>
+      <Route path="/home" element={<Home/>}/>
+      <Route path="/detail/:id" element={<Detail/>}/>
     </Routes>
     </div>
   )
