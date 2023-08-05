f6ad853db8cff550f22e277d1fa28cadb882cfb1
Ezequiel Benitez <125520493+Eze-quielph@users.noreply.github.com>
2023-08-05 14:03:29 -0300

    Post musica casi finalizado

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -3,7 +3,7 @@ import Landing from "./Views/Landing/Landing";
 import Form from "./Views/Login/Login";
 import Home from "./Views/Home/Home";
 import SignUp from "./Views/Register/SignUp";
-import Form_song from "./Views/Form_song/Form_song";
+import FormSong from "./Views/Form_song/Form_song";
 import Detail from "./Views/Detail/Detail";
 import NavBar from "./Components/NavBar/NavBar";
 
@@ -17,7 +17,7 @@ function App() {
             <Route path="/login" element={<Form />} />
             <Route path="/home" element={<Home />} />
             <Route path="/signup" element={<SignUp />} />
-            <Route pathe="/create" element={<Form_song />} />
+            <Route path="/create" element={<FormSong />} />
             <Route path="/detail/:id" element={<Detail />} />
          </Routes>
       </>
--- Client/src/Views/Form_song/Form_song.jsx
+++ Client/src/Views/Form_song/Form_song.jsx
@@ -1,123 +1,97 @@
+import { useDispatch } from "react-redux";
+import { useState, useEffect} from "react";
+import axios from "axios";
 
+const FormSong = () => {
+  const [soundFile, setSoundFile] = useState(null);
+  const [imagedFile, setImageFile] = useState(null);
+  const [data, setData] = useState({
+    url:'',
+    name: 'name',
+    genre: 'Genero',
+    imageUrl: '',
+    isActive: true
+  })
 
-const Form_song = () => {
+  const handleSoundChange = (e) => {
+    setSoundFile(e.target.files[0]);
+  };
+  const handleImageChange = (e) => {
+    setImageFile(e.target.files[0]);
+  };
 
+  const up = async (e) => {
+    e.preventDefault();
+    try {
+      const formSound = new FormData();
+      const formImage = new FormData();
 
-    const [newSong, setNewSong] = useState({
-        name: "",
-        url: "",
-        genre: "",
-    })
+      formSound.append("file", soundFile);
+      formImage.append("multipartFile", imagedFile);
 
-    const [errors, setErrors] = useState({
-        name: "El nombre es obligatorio",
-        url: "La url es obligatoria",
-        genre: "El genero es obligatoria",
-    })
+      const responseSound = await axios.post(
+        "http://localhost:4001/postmusic",
+        formSound
+      );
+      console.log(
+        "URL del archivo musical cargado:",
+        responseSound.data.fileUrl
+      );
 
-
-    const validateName = (input) => {
-        const updateErrors = {
-            ...errors,
-            name: ""
-        }
-        const regex = /^[A-Za-z\s]+$/
-
-
-        if(input.name.length < 5 || input.name.length > 20) updateErrors.name = "El nombre debe tener entre 5 y 20 caracteres"
-
-        setErrors(updateErrors)
-    }
-
-    const validateurl = (input) => {
-        const regex = /^(ftp|http|https):\/\/[^ "]+$/
-
-        const updateErrors = {
-            ...errors,
-            url: ""
-        }
-
-        if(!(regex.test(input.url))) updateErrors.url = "La url debe ser una url"
-
-        setErrors(updateErrors)
-    }
-
-
-    const validategenre = (input) => {
-        const updateError = {
-            ...errors,
-            genre: ""
-        }
-
-        if(input.genre.length < 5 || input.genre.length > 20){
-            updateError.genre = "El genero debe tener entre 5 y 20 caracteres"
-        }
-
-        setErrors(updateError)
-    }
-
-    // const handlesubmit = (e) => {
-    //     e.preventDefault()
-    //     
-    // }
-
-
-    const handleChange = (e) => {
-        setNewSong({
-            ...newSong,
-            [e.target.name]: e.target.value
-        })
-
-        if(e.target.name === "name") validateName({...newSong, [e.target.name]: e.target.value})
-
-        if(e.target.name === "url") validateurl({...newSong, [e.target.name]: e.target.value})
-
-        if(e.target.name === "genre") validategenre({...newSong, [e.target.name]: e.target.value})
-}
-
-    const disabled = () => {
-        let disabled = true;
-        for(let error in errors){
-            if(errors[error] === "") disabled = false;
-            else{
-                disabled = true;
-                break;
+      const responseImage = await axios.post(
+        "https://postimagemicroservice-production.up.railway.app/cloudinary/upload",
+        formImage
+      );
+      console.log(
+          "URL del archivo jpg cargado:",
+        responseImage.data.secure_url
+        );
+        console.log(data);
+       await setData(
+          {...data,
+              imageUrl:responseImage.data.secure_url,
+              url:responseSound.data.fileUrl
+          })
+          const postData = async () => {
+            try {
+              const response = await axios.post(
+                "http://localhost:3001/music/upload/url",
+                data
+              );
+              console.log(response.data);
+            } catch (error) {
+              console.log(error);
             }
-        }
-        return disabled;
+          };
+        
+          postData();
+      console.log(response);
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-
-
-    return (
-        <div>
-
-            <div>
-            <form onSubmit={handlesubmit}>
-                <div>
-                    <label >Nombre: </label>
-                    <input type="text" name="name" placeholder="ingrese nombre" onChange={handleChange}/>
-                    {errors.name && <div>{errors.name}</div>}
-                </div>
-                
-                <div>
-                    <label>Genero: </label>
-                    <input type="text" name="genre" placeholder="ingrese genero de la cancion" onChange={handleChange}/>
-                    {errors.genre && <div>{errors.genre}</div>}
-                </div>
-                
-                <div>
-                    <label>URL: </label>
-                    <input type="text" name="url" placeholder="ingrese url de la cancion" onChange={handleChange}/>
-                    {errors.url && <div>{errors.url}</div>}
-                </div>
-
-                <button type="submit" disabled={disabled()} className={styles.boton}>Crear</button>
-            </form>
-            </div>
-
+  return (
+    <>
+      <form>
+        <div className="form-group">
+          <label htmlFor="file">Seleccione un archivo</label>
+          <input type="file" id="file" onChange={handleSoundChange} />
         </div>
-    )
-}
+        <div className="form-group">
+          <label htmlFor="exampleFormControlFile1">Seleccione una imagen</label>
+          <input
+            type="file"
+            id="exampleFormControlFile1"
+            onChange={handleImageChange}
+          />
+        </div>
+        <button type="button" onClick={(e) => up(e)}>
+          Subir
+        </button>
+      </form>
+    </>
+  );
+};
 
-export default Form_song
+export default FormSong;
