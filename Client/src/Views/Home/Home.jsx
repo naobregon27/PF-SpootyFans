973e3088c892a8ae0d85499c82aa8b3be7d92ac4
ArielBenz1 <ariex1040p@gmail.com>
2023-08-05 23:52:56 -0300

    paginado funcionando

--- Client/src/Views/Home/Home.jsx
+++ Client/src/Views/Home/Home.jsx
@@ -1,13 +1,36 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Cards from '../../Components/Cards/Cards';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import {allSongs} from "../../Redux/actions";
+import Pagination from "../../Components/Pagination/Pagination"
 
 function Home() {
+  const dispatch = useDispatch();
+  const songs = useSelector((state) => state.songs); 
+  const [currentPage, setCurrentPage] = useState(1); 
+  const itemsPerPage = 4;
+
+  useEffect(() => {
+    dispatch(allSongs()); 
+  }, [dispatch]);
+
+  const handlePageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems = songs.slice(indexOfFirstItem, indexOfLastItem);
 
   return (
     <div>
-      <Cards/>
+      <Cards songs={currentItems}/>
+      <Pagination
+        currentPage={currentPage}
+        itemsPerPage={itemsPerPage}
+        totalItems={songs.length}
+        onPageChange={handlePageChange}
+      />
     </div>
   );
 }
