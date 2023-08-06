6b1a7342981e4b4b32d10b24fe55ed4d94e3d5f2
FVazquezRisso <facundovazquez321@gmail.com>
2023-08-06 14:55:29 -0300

    PostMusic testeado

--- Client/src/Views/Form_song/Form_song.jsx
+++ Client/src/Views/Form_song/Form_song.jsx
@@ -1,123 +1,132 @@
+import { useState } from "react";
+import axios from "axios";
 
+const FormSong = () => {
+  const [soundFile, setSoundFile] = useState(null);
+  const [imagedFile, setImageFile] = useState(null);
+  const [data, setData] = useState({
+    url: "",
+    name: "name",
+    genre: "Genero",
+    imageUrl: "",
+    isActive: true,
+  });
 
-const Form_song = () => {
+  const handleSoundChange = (e) => {
+    setSoundFile(e.target.files[0]);
+  };
 
+  const handleImageChange = (e) => {
+    setImageFile(e.target.files[0]);
+  };
 
-    const [newSong, setNewSong] = useState({
-        name: "",
-        url: "",
-        genre: "",
-    })
+  const handleNameChange = (e) => {
+    setData({
+      ...data,
+      name: e.target.value,
+    });
+  };
 
-    const [errors, setErrors] = useState({
-        name: "El nombre es obligatorio",
-        url: "La url es obligatoria",
-        genre: "El genero es obligatoria",
-    })
+  const handleGenreChange = (e) => {
+    setData({
+      ...data,
+      genre: e.target.value,
+    });
+  };
 
-
-    const validateName = (input) => {
-        const updateErrors = {
-            ...errors,
-            name: ""
-        }
-        const regex = /^[A-Za-z\s]+$/
-
-
-        if(input.name.length < 5 || input.name.length > 20) updateErrors.name = "El nombre debe tener entre 5 y 20 caracteres"
-
-        setErrors(updateErrors)
+  const postData = async (postData) => {
+    try {
+      const response = await axios.post(
+        "http://localhost:3001/music/upload/url",
+        postData
+      );
+      console.log(response.data);
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-    const validateurl = (input) => {
-        const regex = /^(ftp|http|https):\/\/[^ "]+$/
+  const up = async (e) => {
+    e.preventDefault();
+    try {
+      const formSound = new FormData();
+      const formImage = new FormData();
 
-        const updateErrors = {
-            ...errors,
-            url: ""
-        }
+      formSound.append("file", soundFile);
+      formImage.append("multipartFile", imagedFile);
 
-        if(!(regex.test(input.url))) updateErrors.url = "La url debe ser una url"
+      const responseSound = await axios.post(
+        "http://localhost:4001/postmusic",
+        formSound
+      );
+      console.log(
+        "URL del archivo musical cargado:",
+        responseSound.data.fileUrl
+      );
 
-        setErrors(updateErrors)
+      const responseImage = await axios.post(
+        "https://postimagemicroservice-production.up.railway.app/cloudinary/upload",
+        formImage
+      );
+      console.log(
+        "URL del archivo jpg cargado:",
+        responseImage.data.secure_url
+      );
+
+      const postDataObject = {
+        ...data,
+        imageUrl: responseImage.data.secure_url,
+        url: responseSound.data.fileUrl,
+      };
+
+      await setData(postDataObject);
+      await postData(postDataObject);
+
+      console.log(response);
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-
-    const validategenre = (input) => {
-        const updateError = {
-            ...errors,
-            genre: ""
-        }
-
-        if(input.genre.length < 5 || input.genre.length > 20){
-            updateError.genre = "El genero debe tener entre 5 y 20 caracteres"
-        }
-
-        setErrors(updateError)
-    }
-
-    // const handlesubmit = (e) => {
-    //     e.preventDefault()
-    //     
-    // }
-
-
-    const handleChange = (e) => {
-        setNewSong({
-            ...newSong,
-            [e.target.name]: e.target.value
-        })
-
-        if(e.target.name === "name") validateName({...newSong, [e.target.name]: e.target.value})
-
-        if(e.target.name === "url") validateurl({...newSong, [e.target.name]: e.target.value})
-
-        if(e.target.name === "genre") validategenre({...newSong, [e.target.name]: e.target.value})
-}
-
-    const disabled = () => {
-        let disabled = true;
-        for(let error in errors){
-            if(errors[error] === "") disabled = false;
-            else{
-                disabled = true;
-                break;
-            }
-        }
-        return disabled;
-    }
-
-
-
-    return (
-        <div>
-
-            <div>
-            <form onSubmit={handlesubmit}>
-                <div>
-                    <label >Nombre: </label>
-                    <input type="text" name="name" placeholder="ingrese nombre" onChange={handleChange}/>
-                    {errors.name && <div>{errors.name}</div>}
-                </div>
-                
-                <div>
-                    <label>Genero: </label>
-                    <input type="text" name="genre" placeholder="ingrese genero de la cancion" onChange={handleChange}/>
-                    {errors.genre && <div>{errors.genre}</div>}
-                </div>
-                
-                <div>
-                    <label>URL: </label>
-                    <input type="text" name="url" placeholder="ingrese url de la cancion" onChange={handleChange}/>
-                    {errors.url && <div>{errors.url}</div>}
-                </div>
-
-                <button type="submit" disabled={disabled()} className={styles.boton}>Crear</button>
-            </form>
-            </div>
-
+  return (
+    <>
+      <form>
+        <div className="form-group">
+          <label htmlFor="file">Seleccione un archivo</label>
+          <input type="file" id="file" onChange={handleSoundChange} />
         </div>
-    )
-}
+        <div className="form-group">
+          <label htmlFor="exampleFormControlFile1">Seleccione una imagen</label>
+          <input
+            type="file"
+            id="exampleFormControlFile1"
+            onChange={handleImageChange}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="name">Nombre de la canción</label>
+          <input
+            type="text"
+            id="name"
+            value={data.name}
+            onChange={handleNameChange}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="genre">Género</label>
+          <input
+            type="text"
+            id="genre"
+            value={data.genre}
+            onChange={handleGenreChange}
+          />
+        </div>
+        <button type="button" onClick={up}>
+          Subir
+        </button>
+      </form>
+    </>
+  );
+};
 
-export default Form_song
+export default FormSong;
