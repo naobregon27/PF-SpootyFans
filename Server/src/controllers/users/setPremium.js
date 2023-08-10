83020b9560649136ed63ac06d2525c7a2287fb21
FVazquezRisso <facundovazquez321@gmail.com>
2023-08-10 09:12:52 -0300

    Se agregó la ruta setPremium
    Se agregó una ruta "/user/setPremium" que cambia el estado premium de un usuario.

---
+++ Server/src/controllers/users/setPremium.js
@@ -0,0 +1,25 @@
+const { User } = require("../../db");
+
+const setPremium = async (userId) => {
+  try {
+    if (!userId) throw new Error("Datos insuficientes.");
+
+    const userFound = await User.findByPk(userId);
+
+    if (!userFound)
+      throw new Error(`No se ha encontrado el usuario con la id: ${userId}.`);
+
+    const userModified = await User.update(
+      { isPremium: !userFound.isPremium },
+      { where: { id: userId } }
+    );
+
+    if (!userModified) throw new Error("Error al modificar el usuario.");
+
+    return "Usuario modificado correctamente.";
+  } catch (error) {
+    return { error: error.message };
+  }
+};
+
+module.exports = setPremium;
