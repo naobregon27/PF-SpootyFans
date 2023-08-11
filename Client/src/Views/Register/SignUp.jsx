3e113620aa4630ecda1dcc7e2f4e4dd7509f6476
FVazquezRisso <facundovazquez321@gmail.com>
2023-08-11 09:32:39 -0300

    Update SignUp.jsx

--- Client/src/Views/Register/SignUp.jsx
+++ Client/src/Views/Register/SignUp.jsx
@@ -10,7 +10,7 @@ const SignUp = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
-  const [isChecked, setIsChecked] = useState(false);
+  // const [isChecked, setIsChecked] = useState(false);
   const [errors, setErrors] = useState({});
 
   const navigate = useNavigate();
@@ -34,8 +34,6 @@ const SignUp = () => {
         username,
         email,
         password,
-        isActive: true,
-        isPremium: true,
       });
 
       navigate("/login");
@@ -62,9 +60,9 @@ const SignUp = () => {
     if (password !== confirmPassword) {
       errors.confirmPassword = "Passwords do not match";
     }
-    if (!isChecked) {
-      errors.isChecked = "You must agree to the terms and conditions";
-    }
+    // if (!isChecked) {
+    //   errors.isChecked = "You must agree to the terms and conditions";
+    // }
     return errors;
   };
 
@@ -154,128 +152,6 @@ const SignUp = () => {
                 </p>
               )}
             </label>
-
-            {/* <label>
-  const [username, setUsername] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
-  // const [isChecked, setIsChecked] = useState(false);
-  const [errors, setErrors] = useState({});
-
-  const navigate = useNavigate();
-
-  const createUser = async (signUp) => {
-    try {
-      const response = await axios.post(
-        "http://localhost:3001/user/register",
-        signUp
-      );
-      alert("User created successfully");
-    } catch (error) {
-      console.error(error);
-    }
-  };
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    const errors = validate();
-    if (Object.keys(errors).length === 0) {
-      await createUser({
-        username,
-        email,
-        password,
-      });
-
-      navigate("/login");
-    } else {
-      setErrors(errors);
-    }
-  };
-
-  const validate = () => {
-    const errors = {};
-    if (!username) {
-      errors.username = "Username is required";
-    }
-    if (!email) {
-      errors.email = "Email is required";
-    } else if (!/\S+@\S+\.\S+/.test(email)) {
-      errors.email = "Email is invalid";
-    }
-    if (!password) {
-      errors.password = "Password is required";
-    } else if (password.length < 6) {
-      errors.password = "Password must be at least 6 characters";
-    }
-    if (password !== confirmPassword) {
-      errors.confirmPassword = "Passwords do not match";
-    }
-    // if (!isChecked) {
-    //   errors.isChecked = "You must agree to the terms and conditions";
-    // }
-    return errors;
-  };
-
-  return (
-    <main className={style.mainContainer}>
-      <img className={style.logo} src={logo}></img>
-      <div className={style.log}>
-        <div className={style.title_container}>
-          <h1 className={style.title}>Register</h1>
-          <Link to="/login" className={style.to_login}>
-            or Login
-          </Link>
-        </div>
-        <form onSubmit={handleSubmit} className={style.form_container}>
-          <label>
-            <input
-              className={style.datos}
-              type="text"
-              value={username}
-              placeholder="Username"
-              onChange={(event) => setUsername(event.target.value)}
-            />
-            {errors.username && <span>{errors.username}</span>}
-          </label>
-          <label>
-            <input
-              className={style.datos}
-              type="email"
-              value={email}
-              placeholder="Email"
-              onChange={(event) => setEmail(event.target.value)}
-            />
-            {errors.email && <span>{errors.email}</span>}
-          </label>
-          <label>
-            <input
-              className={style.datos}
-              type="password"
-              value={password}
-              placeholder="Password"
-              onChange={(event) => setPassword(event.target.value)}
-            />
-            {errors.password && <span>{errors.password}</span>}
-          </label>
-          <label>
-            <input
-              className={style.datos}
-              type="password"
-              value={confirmPassword}
-              placeholder="Confirm Password"
-              onChange={(event) => setConfirmPassword(event.target.value)}
-            />
-            {errors.confirmPassword && <span>{errors.confirmPassword}</span>}
-          </label>
-          {/* <label>
-            <input
-              type="checkbox"
-              checked={isChecked}
-              onChange={(event) => setIsChecked(event.target.checked)}
-            />
-            I agree to the terms and conditions
-            {errors.isChecked && <span>{errors.isChecked}</span>}
-          </label> */}
             <button
               className="flex flex-col justify-center items-center bg-white border w-[7rem] h-[2.3rem] rounded-[5rem] mt-2 mb-5  hover:scale-[1.1] hover:bg-black hover:text-[#fff] duration-[.3s]"
               type="submit"
