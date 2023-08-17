10bc3cd450e69714c262e98820e689cb787a5085
T_NahuelReboledo <thiagoreboledo8@gmail.com>
2023-08-16 23:57:57 -0300

    se agrego un efecto 3d, pero cambie el fondo no me maten

--- Client/src/Views/Landing/Landing.jsx
+++ Client/src/Views/Landing/Landing.jsx
@@ -8,15 +8,52 @@ function Landing() {
 
    useEffect(() => {
       dispatch(allSongs());
+
+      const el = document.getElementById("Card_Landing");
+      const height = el.clientHeight;
+      const width = el.clientWidth;
+
+      const handleMouseMove = (e) => {
+         const { layerX, layerY } = e;
+
+         const yRotation = ((layerX - width / 2) / width) * 20;
+         const xRotation = ((layerY - height / 2) / height) * 20;
+
+         const string = `
+        perspective(500px)
+        scale(1.1)
+        rotateX(${xRotation}deg)
+        rotateY(${yRotation}deg)`;
+
+         el.style.transform = string;
+      };
+
+      const handleMouseOut = () => {
+         el.style.transform = `
+        perspective(500px)
+        scale(1)
+        rotateX(0)
+        rotateY(0)`;
+      };
+
+      el.addEventListener("mousemove", handleMouseMove);
+      el.addEventListener("mouseout", handleMouseOut);
+
+      return () => {
+         el.removeEventListener("mousemove", handleMouseMove);
+         el.removeEventListener("mouseout", handleMouseOut);
+      };
    }, []);
 
    return (
       <div className="flex flex-col items-center justify-center w-screen h-screen bg-transparent font-custom overflow-hidden text-white">
-
-         <div className="flex flex-col items-center justify-center h-[17rem] w-[30rem] backdrop-blur-[10px] bg-[#ffffff10] shadow-inner shadow-white rounded-[1rem] p-5 z-10">
-
-            <h1 className="text-[2.5rem] leading-3 select-none">Welcome</h1>
-            <h1 className="text-[2.5rem] select-none">fans of new music!</h1>
+         <div
+            id="Card_Landing"
+            className="flex flex-col items-center justify-evenly h-[17rem] w-[30rem] backdrop-blur-[6px] bg-[#ffffff10] shadow-inner shadow-white rounded-[1rem] p-5 z-10">
+            <div className="flex flex-col justify-center items-center">
+               <h1 className="text-[3rem] leading-3 select-none">Welcome</h1>
+               <h1 className="text-[2.5rem] select-none">fans of new music!</h1>
+            </div>
 
             <div className="grid grid-cols-3">
                <NavLink
@@ -34,7 +71,6 @@ function Landing() {
                </NavLink>
             </div>
          </div>
-         <img className="w-70 max-ms:hidden" src="" alt="" />
       </div>
    );
 }
