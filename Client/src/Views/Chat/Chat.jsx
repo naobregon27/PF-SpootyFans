e444b3cdd1accc4e34a3f2738ee9a70f6e3a5858
T_NahuelReboledo <thiagoreboledo8@gmail.com>
2023-08-21 16:30:45 -0300

    acomodo chat

--- Client/src/Views/Chat/Chat.jsx
+++ Client/src/Views/Chat/Chat.jsx
@@ -1,59 +1,73 @@
-import { io } from "socket.io-client"
-import { useState, useEffect } from "react"
+import { io } from "socket.io-client";
+import { useState, useEffect } from "react";
 import { LiMensaje, UlMensajes } from "./plantillachat.jsx";
-import style from "./chat.module.css";
-const socket = io('http://localhost:3002');
-import {IconSend} from "@tabler/icons-react";
+const socket = io("http://localhost:3002");
+import { IconSend } from "@tabler/icons-react";
 
 function Chat() {
+   const [isConnected, setIsConnected] = useState(false);
+   const [nuevoMensaje, setNuevoMensaje] = useState("");
+   const [mensajes, setMensajes] = useState([]);
 
-  const [isConnected, setIsConnected] = useState(false);
-  const [nuevoMensaje, setNuevoMensaje] = useState('');
-  const [mensajes, setMensajes] = useState([]);
+   useEffect(() => {
+      socket.on("connect", () => setIsConnected(true));
 
-  useEffect(() => {
+      socket.on("chat_message", (data) => {
+         setMensajes((mensajes) => [...mensajes, data]);
+      });
 
-    socket.on('connect', () => setIsConnected(true));
+      return () => {
+         socket.off("connect");
+         socket.off("chat_message");
+      };
+   }, []);
 
-    socket.on('chat_message', (data) => {
-      setMensajes(mensajes => [...mensajes, data]);
-    });
+   const enviarMensaje = () => {
+      socket.emit("chat_message", {
+         // usuario: socket.id,
+         mensaje: nuevoMensaje,
+      });
+   };
 
-    return () => {
-      socket.off('connect');
-      socket.off('chat_message');
-    }
+   return (
+      <div className="font-custom w-full h-[32rem] mt-[-3rem] max-md:mt-10 max-md:mb-5">
+         {isConnected ? (
+            <div className="flex flex-row justify-center">
+               <p>online&nbsp;</p>
+               <span className=" rounded-full w-[1rem] h-[1rem] bg-green-600">&nbsp;</span>
+            </div>
+         ) : (
+            <div className="flex justify-center overflow-hidden">
+               <p>offline&nbsp;</p>
+               <span className=" rounded-full w-[1rem] h-[1rem] bg-red-600">&nbsp;</span>
+            </div>
+         )}
 
-  }, []);
+         <div className="h-full overflow-x-hidden overflow-y-scroll border border-[#ffffff20] m-1 rounded-l-[1rem] bg-[#ffffff10] backdrop-blur-[10px]">
+            <UlMensajes>
+               {mensajes.map((mensaje) => (
+                  <LiMensaje>
+                     {" "}
+                     {mensaje.usuario} {mensaje.mensaje}
+                  </LiMensaje>
+               ))}
+            </UlMensajes>
+         </div>
 
-  const enviarMensaje = () => {
-    socket.emit('chat_message', {
-      // usuario: socket.id,
-      mensaje: nuevoMensaje
-    });
-  }
+         <div
+            className=" border border-[#ffffff30] flex flex-row justify-center items-center rounded-[5rem] backdrop-blur-3xl"
+            type="text"
+            value={nuevoMensaje}
+            onChange={(e) => setNuevoMensaje(e.target.value)}>
 
-  return (
-    <div className="font-custom">
-      <h2 className="pl-20">{isConnected ? "online..." : "offline"}</h2>
-      <div className={style.mensaje}>
-        <UlMensajes>
-          {mensajes.map(mensaje => (
-            <LiMensaje> {mensaje.usuario} {mensaje.mensaje}</LiMensaje>
-          ))}
-        </UlMensajes>
+            <input className="bg-transparent w-full pl-3 outline-none"/>
+
+            <button className="p-[.4rem]" onClick={enviarMensaje}>
+               <IconSend size={25}/>
+            </button>
+         </div>
       </div>
-
-      <div className= "shadow-inner shadow-white backdrop-blur-[5px] flex flex-row border h-[2.5rem] bg-transparent rounded-[5rem] focus:bg-[#ffffff20]" type="text" value={nuevoMensaje} onChange={e => setNuevoMensaje(e.target.value)} >
-        <input className={style.input}/>
-        <button className="pl-1 outline-none"onClick={enviarMensaje}><IconSend size={30}/></button>
-      </div>
-  
-      <div>
-      
-      </div>
-    </div>
-  );
+   );
 }
 
 export default Chat;
--- Client/src/Views/Chat/plantillachat.jsx
+++ Client/src/Views/Chat/plantillachat.jsx
@@ -1,22 +1,20 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 const UlMensajes = styled.ul`
-    max-width: 800px;
-    margin: 10px auto;
-    list-style: none;
-    display: flex;
-    flex-direction: column;
-    gap: 5px;
+   margin: 10px auto;
+   list-style: none;
+   display: flex;
+   flex-direction: column;
 `;
 
 const LiMensaje = styled.li`
-    background-color: #00000080;
-    padding: .5rem .8rem;
-    border-radius: 2rem;
-    margin: .5rem;
-    color: white;
-`
+   background-color: #ffffff30;
+   border: solid 1px #ffffff50;
+   box-shadow: 0px 15px 60px 0px rgba(0,0,0,1);
+   padding: 0.3rem 0.8rem;
+   border-radius: 2rem;
+   margin: 0.1rem .5rem;
+   color: white;
+`;
 
-export {
-    UlMensajes, LiMensaje
-}
+export { UlMensajes, LiMensaje };
