74641c661e90688bb91759c4be418d0d354f47c0
FVazquezRisso <130169863+FVazquezRisso@users.noreply.github.com>
2023-08-24 17:37:00 -0300

    Ultima prueba

--- Client/src/Views/Chat/Chat.jsx
+++ Client/src/Views/Chat/Chat.jsx
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 import { useState, useEffect } from "react";
 import { LiMensaje, UlMensajes } from "./plantillachat.jsx";
-const socket = io("https://spootyfans.onrender.com");
+const socket = io("https://spootyfans.onrender.com:80");
 
 import { IconSend } from "@tabler/icons-react";
 
